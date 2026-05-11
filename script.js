775a3575ec135bd0905abd6a08ef23b547601379
labqnt <labqnt@gmail.com>
2026-05-11 20:38:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const obs = new IntersectionObserver(entries => {
 }, { threshold: 0.1 });
 
 document.querySelectorAll(".fade-in").forEach(el => obs.observe(el));
+
+function toggleMobileMenu() {
+  const navLinks = document.getElementById("navLinks");
+  if (navLinks) {
+    navLinks.classList.toggle("open");
+  }
+}
