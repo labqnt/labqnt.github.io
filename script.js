bf491400e9b824e7534efbd80c1853129a891828
labqnt <labqnt@gmail.com>
2026-05-11 20:12:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const obs = new IntersectionObserver(entries => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add("visible");
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll(".fade-in").forEach(el => obs.observe(el));
